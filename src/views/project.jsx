ffea3c0488ea5a281305a881fd0ccd6a576b83dd
spring-raining <fdonthavei@gmail.com>
2015-12-01 02:29:24 +0900

    Giraf logo

--- src/views/project.jsx
+++ src/views/project.jsx
@@ -1,46 +1,84 @@
 "use strict";
 
 import React              from "react";
+import Isvg               from "react-inlinesvg";
 
+import pkg                from "package.json";
 import Actions            from "src/actions/actions";
 import Item               from "src/views/project/item";
+import Menu               from "src/views/menu";
+import genUUID            from "src/utils/genUUID";
 
 
 var Project = React.createClass({
-  render() {
-    let store = this.props.store;
 
-    let footages = store.get("footages").map((e) => {
-      let isSelected = (e.id === store.get("selectingItem", "id"));
+  menuContent: [
+    {
+      name: "ファイルを読み込む",
+      onClick: () => {
+        Actions.updateExpandingMenuId(null);
+        Actions.importFile();
+      },
+    }, {
+      name: "コンポジションを作成",
+      onClick: () => {
+        Actions.updateExpandingMenuId(null);
+        Actions.createComposition();
+      },
+    }
+  ],
+
+  menuId: genUUID(),
+
+  render() {
+    const store = this.props.store;
+    const expandMenuId = store.get("expandingMenuId");
+
+    const footages = store.get("footages").map((e) => {
+      const isSelected = (e.id === store.get("selectingItem", "id"));
       return <Item item={e} key={e.id}
                    isSelected={isSelected}
                    isEdited={false} />;
     });
-    let comps = store.get("compositions").map((e) => {
-      let isSelected = (e.id === store.get("selectingItem", "id"));
-      let isEdited   = (e.id === store.get("editingComposition", "id"));
+    const comps = store.get("compositions").map((e) => {
+      const isSelected = (e.id === store.get("selectingItem", "id"));
+      const isEdited   = (e.id === store.get("editingComposition", "id"));
       return <Item item={e} key={e.id}
                    isSelected={isSelected}
                    isEdited={isEdited} />;
     });
 
-    return <section className="project panel">
-        <button onClick={this._onImportFileButtonClicked}>ファイルを読み込む</button>
-        <button onClick={this._onCreateCompositionButtonClicked}>Create Composition</button>
-        <ul>
+    return (
+      <section className="project panel">
+        <div className="project__brand">
+          <Isvg src="rsc/giraf_logo.svg"
+                className="project__brand__logo giraf_logo"/>
+          <div className="project__brand__text">
+            <span className="project__brand__version">{pkg.version}</span>
+            <span className="project__brand__copyright">{pkg.copyright}</span>
+          </div>
+          <button className="project__brand__menu flat"
+                  onClick={this._onMenuButtonClick}>
+            ■
+            <Menu content={this.menuContent}
+                  expand={expandMenuId === this.menuId}/>
+          </button>
+        </div>
+        <ul className="project__item-ul">
           {footages}
           {comps}
         </ul>
-      </section>;
+      </section>
+    );
   },
 
-  _onImportFileButtonClicked() {
-    Actions.importFile();
-  },
-
-  _onCreateCompositionButtonClicked() {
-    Actions.createComposition();
-  },
+  _onMenuButtonClick(e) {
+    e.stopPropagation();
+    const expandingMenuId = this.props.store.get("expandingMenuId");
+    Actions.updateExpandingMenuId(
+      (expandingMenuId === this.menuId)? null : this.menuId
+    );
+  }
 });
 
 export default Project;

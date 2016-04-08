f037025a2d72a8262d2f5477fe564595bc85c316
spring-raining <fdonthavei@gmail.com>
2016-04-08 19:08:44 +0900

    Upgrade react-intl version

--- src/giraf.jsx
+++ src/giraf.jsx
@@ -3,8 +3,8 @@
 import React                      from "react";
 import ReactDOM                   from "react-dom";
 import {addLocaleData, IntlProvider} from "react-intl";
-import enLocaleData               from "react-intl/lib/locale-data/en";
-import jaLocaleData               from "react-intl/lib/locale-data/ja";
+import enLocaleData               from "react-intl/locale-data/en";
+import jaLocaleData               from "react-intl/locale-data/ja";
 
 import App                        from "src/views/app";
 import getUserLanguage            from "src/utils/getUserLanguage";
